fdbdccb1178829cbe084a666fbdc052516f468af
Tony Ho <ho.tonym@gmail.com>
2018-04-26 03:17:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog
+
+  def initialize(dog_name)
+    @name = dog_name
+    @bark = "woof!"
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+end
